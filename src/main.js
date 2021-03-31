16fd7be03a166e06a8441b55bc7c64c9727ea0fd
alex.luo <alex.luo@itnsl.net>
2021-03-31 10:46:36 +0800

    test

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 // import App from './App.vue'
 import home from './home.vue';
-import router from './router'
+import router from './router.js'
 
 const app = createApp(home)
 app.use(router)
--- src/router.js
+++ src/router.js
@@ -1,20 +1,16 @@
 import { createWebHistory, createRouter } from "vue-router";
-import home from "@/components/Container.vue"
-// import detail from "@/pages/detail.vue"
 
 const routes = [{
         path: "/",
         name: "Home",
-        component: home
-        // component: () =>
-        //     import ("@/components/Container.vue"),
+        component: () =>
+            import ("@/components/Container.vue"),
     },
     {
         path: "/detail/:name",
         name: "detail",
-        component: () => import(/* webpackChunkName: "about" */ '@/pages/detail.vue'),
-        // component: () =>
-        //     import ("@/pages/detail.vue"),
+        component: () =>
+            import ("@/pages/detail.vue"),
         props: true
     }
 ];
