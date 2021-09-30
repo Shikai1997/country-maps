68a297bd219eaac346e82db8f45fd80de1533fb9
Alex <56082441+Shikai1997@users.noreply.github.com>
2021-09-30 17:41:41 +0800

    router catchAll

--- src/router.js
+++ src/router.js
@@ -14,6 +14,10 @@ const routes = [
         component: () =>
             import ("./pages/detail.vue"),
         props: true
+    },
+    {
+        path: "/:catchAll(.*)",
+        redirect: 'home'
     }
 ];
 
